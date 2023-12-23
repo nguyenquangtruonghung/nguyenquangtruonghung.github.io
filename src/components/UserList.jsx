9f00d43c0265ba84967670568825776d2fea65dc
nguyenquangtruonghung <nguyenquangtruonghung99@gmail.com>
2023-12-23 22:15:18 +0700

    Change GET endpoint

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -7,7 +7,7 @@ const UserList = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://ap-southeast-1.aws.data.mongodb-api.com/app/data-oetyj/endpoint/getUserTest');
+        const response = await axios.get('http://192.168.1.207:3001/api/data');
         const decodedData = response.data.data.map(user => {
           const base64Data = user.body.Data;
           const decodedUserData = atob(base64Data);
