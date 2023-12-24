20a837ade91cd9c964caddd1685debe50ba0eab2
nguyenquangtruonghung <nguyenquangtruonghung99@gmail.com>
2023-12-25 00:30:14 +0700

    Update changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,36 @@
-import React, { useState } from 'react';
-import Form from '@rjsf/core';
-import validator from '@rjsf/validator-ajv8';
-import axios from 'axios'; 
+import React, { useState, useEffect } from "react";
+import Form from "@rjsf/core";
+import validator from "@rjsf/validator-ajv8";
+import axios from "axios";
+import "./UserLogin.css"; // Import file CSS tùy chỉnh
 
 const schema = {
-  title: 'SIGN UP',
-  type: 'object',
-  required: ['username', 'password'],
+  title: "NHẬP THÔNG TIN ĐIỀU KHIỂN HỆ THỐNG",
+  type: "object",
+  required: ["desire"],
   properties: {
-    username: { type: 'string', title: 'Username' },
-    password: { type: 'string', title: 'Password', format: 'password' },
+    desire: { type: "number", title: "Nhập khoảng cách mong muốn (mm)" },
   },
+};
+
+const TimeDisplay = () => {
+  const [time, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      <p style={{ fontSize: "1.2em" }}>
+        Thời gian hiện tại: {time.toLocaleTimeString()}
+      </p>
+    </div>
+  );
 };
 
 const UserLogin = () => {
@@ -18,19 +38,24 @@ const UserLogin = () => {
 
   const handleSubmit = async ({ formData }) => {
     try {
-      console.log('Dữ liệu gửi đi:', formData); // Log dữ liệu trước khi gửi để kiểm tra
+      const dataWithTime = {
+        ...formData,
+        time: new Date().toISOString(),
+      };
 
+      console.log("Dữ liệu gửi đi:", dataWithTime);
+
+      // Gửi POST request đến endpoint thứ nhất
       const response = await axios.post(
-        'https://eastasia.azure.data.mongodb-api.com/app/application-0-hlnel/endpoint/postForm',
-        formData
+        "https://us-east-1.aws.data.mongodb-api.com/app/agg_func-voayj/endpoint/update_input",
+        dataWithTime
       );
 
-      console.log('Kết quả từ server:', response.data);
+      console.log("Kết quả từ server:", response.data);
 
-      // Reset form sau khi submit thành công
       setFormData({});
     } catch (error) {
-      console.error('Lỗi khi gửi dữ liệu:', error);
+      console.error("Lỗi khi gửi dữ liệu:", error);
     }
   };
 
@@ -39,10 +64,11 @@ const UserLogin = () => {
       <Form
         schema={schema}
         validator={validator}
-        formData={formData} // Truyền giá trị của form
-        onChange={({ formData }) => setFormData(formData)} // Cập nhật state khi form thay đổi
+        formData={formData}
+        onChange={({ formData }) => setFormData(formData)}
         onSubmit={handleSubmit}
       />
+      <TimeDisplay />
     </div>
   );
 };
