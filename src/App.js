09a33dd1c7bd2aec01394876d53636511e48c470
nguyenquangtruonghung <nguyenquangtruonghung99@gmail.com>
2023-12-25 04:34:19 +0700

    Update Project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-/* import './App.css';
+import './App.css';
 import Form from './components/Form';
 import UserList from './components/UserList';
 function App() {
@@ -13,38 +13,4 @@ function App() {
   );
 }
 
-export default App; */
-
-// App.js
-import React, { useState } from 'react';
-import './App.css';
-import AuthPage from './components/AuthPage';
-import Form from './components/Form';
-import UserList from './components/UserList';
-
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLoginSuccess = () => {
-    setIsLoggedIn(true);
-  };
-
-  return (
-    <div className="App">
-      <header>
-        <h1>SINGLE CYLINDER SOLENOID ENGINE</h1>
-        {isLoggedIn ? (
-          <>
-            <Form />
-            <UserList />
-          </>
-        ) : (
-          <AuthPage onLoginSuccess={handleLoginSuccess} />
-        )}
-      </header>
-    </div>
-  );
-}
-
 export default App;
-
